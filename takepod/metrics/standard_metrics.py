6d822049d9734268512cc4b918d0b643d71c2171
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2018-10-03 15:33:40 +0200

    added metric, added verbosity param to dataset
    Added f1 multiclass metric wrapper, added verbosity parameter to pauzahr
    dataset

--- takepod/metrics/standard_metrics.py
+++ takepod/metrics/standard_metrics.py
@@ -3,3 +3,7 @@ from sklearn.metrics import f1_score
 
 def f1(true, pred):
     return f1_score(true, pred)
+
+def f1_multiclass(true, pred):
+    ##TODO:uncomplete, missing one argument to be able to use in gridsearchcv
+    return f1_score(y_true = true, y_pred=pred, average='macro')
