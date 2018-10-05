05bbedc530e89de58e6057e72c6f4646eb1affe1
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2018-10-05 14:38:24 +0200

    Added downloader file

---
+++ takepod/storage/downloader.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+import requests
+from tqdm import tqdm
+
+class BaseDownloader(ABC):
+    @abstractmethod
+    def download (self, url, path):
+        pass
+
+class HttpDownloader(BaseDownloader, ABC):
+    def _process_response(self, path, http_response):
+        chunk_size = 16 * 1024
+        total_size = int(http_response.headers.get('Content-length', 0))
+        with open(path, "wb") as file:
+            with tqdm(total=total_size, unit='B',
+                      unit_scale=1, desc=path.split('/')[-1]) as t:
+                for chunk in http_response.iter_content(chunk_size):
+                    if chunk:
+                        file.write(chunk)
+                        t.update(len(chunk))
+
+class UrlDownloader(HttpDownloader):
+    def download(self, url, path):
+        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, stream=True)
+        self._process_response(path, response)
+
+class GoogleDriveDownloader(HttpDownloader):
+    def download(self, url, path):
+        if 'drive.google.com' not in url:
+            raise ValueError("not supported url type")
+        confirm_token = None
+        session = requests.Session()
+        response = session.get(url, stream=True)
+        for k, v in response.cookies.items():
+            if k.startswith("download_warning"):
+                confirm_token = v
+
+        if confirm_token:
+            url = url + "&confirm=" + confirm_token
+            response = session.get(url, stream=True)
+
+        self._process_response(path, response)
+
+    
