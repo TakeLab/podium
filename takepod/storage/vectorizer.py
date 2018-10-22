9e7544faa8c38120a55ded2df772b4921851e588
Domagoj Pluscec <domagoj.pluscec@fer.hr>
2018-10-22 12:51:30 +0200

    vectorizer interface (#11)
    Created VectorStorage base class and simple implementation. VectorStorage is used for loading vectors such as word2vec and glove from memory.
    
    Test results: 
    
    ```
    ----------- coverage: platform win32, python 3.6.2-final-0 -----------
    Name                                          Stmts   Miss  Cover   Missing
    ---------------------------------------------------------------------------
    takepod\__init__.py                               1      0   100%
    takepod\dataload\__init__.py                      1      0   100%
    takepod\dataload\imdb.py                         28      1    96%   19
    takepod\dataload\pauzahr.py                      39     12    69%   16-30
    takepod\metrics\__init__.py                       3      0   100%
    takepod\models\__init__.py                        0      0   100%
    takepod\models\base\__init__.py                   0      0   100%
    takepod\models\base_model.py                     11      3    73%   8, 15, 19
    takepod\models\simple_sentiment_analysis.py      50      0   100%
    takepod\preproc\__init__.py                       0      0   100%
    takepod\preproc\stemmer\__init__.py               0      0   100%
    takepod\preproc\stemmer\croatian_stemmer.py      35      0   100%
    takepod\preproc\transform.py                     16      0   100%
    takepod\storage\__init__.py                       0      0   100%
    takepod\storage\downloader.py                    27      2    93%   44, 72
    takepod\storage\utility.py                        8      0   100%
    takepod\storage\vectorizer.py                    91      6    93%   78, 101, 127, 245, 257-258
    ---------------------------------------------------------------------------
    TOTAL                                           310     24    92%
    
    ===================== 44 passed in 3.32 seconds ========================
    ```

---
+++ takepod/storage/vectorizer.py
@@ -0,0 +1,298 @@
+"""Module vectorizer offers classes for vectorizing tokens.
+ Interface of implemented concrete vectorizers is given in Vectorizer class.
+
+"""
+import os
+from abc import ABC, abstractmethod
+import six
+import numpy as np
+
+
+def zeros_default_vector(token, dim):
+    """Function for creating default vector for given token in form of zeros
+    array. Dimension of returned array is equal to given dim.
+
+    Parameters
+    ----------
+    token : str
+        string token from vocabulary
+    dim : int
+        vector dimension
+
+    Returns
+    -------
+    vector : array-like
+        zeros vector with given dimension
+    """
+    if dim is None:
+        raise ValueError("Dim mustn't be None,"
+                         " given token={}, dim={}".format(token, dim))
+    return np.zeros(dim)
+
+
+class VectorStorage(ABC):
+    """Interface for classes that can vectorize token. One example of such
+    vectorizer is word2vec.
+
+    """
+
+    def __init__(self, path, default_vector_function=None,
+                 cache_path=None, max_vectors=None):
+        """ Vectorizer base class constructor.
+
+        Parameters
+            ----------
+            path : str
+                path to stored vectors
+            default_vector_function : callable, optional
+                which vector should be returned if vectorizer doesn't have
+                representation for given token. If None and token doesn't
+                exists an error is raised while obtaining a vector
+            cache_path : str, optional
+                path to cached vectors. Caching vectors should be used when
+                using vocab for loading vectors or when limiting number of
+                vectors to load
+            max_vectors : int, optional
+                maximal number of vectors to load in the memory
+
+        """
+        self._path = path
+        self._default_vector_function = default_vector_function
+        self._cache_path = cache_path
+        self._max_vectors = max_vectors
+
+    @abstractmethod
+    def load_all(self):
+        """Method loads all vectors stored in instance path to the vectors.
+
+        Raises
+        ------
+        IOError
+            if there was a problem while reading vectors from instance path
+        ValueError
+            if instance path is not a valid path
+        RuntimeError
+            if different vector size is detected while loading vectors
+
+        """
+        pass
+
+    @abstractmethod
+    def load_vocab(self, vocab):
+        """Method loads vectors for tokens in vocab
+        stored in given path to the instance.
+
+        Parameters
+        ----------
+        vocab : iterable object
+            vocabulary with unique words
+
+        Raises
+        ------
+        IOError
+            if there was a problem while reading vectors from instance path
+        ValueError
+            if given path is not a valid path or given vocab is none
+            or if the vector values in vector storage cannot be casted to float
+        RuntimeError
+            if different vector size is detected while loading vectors
+
+        """
+        pass
+
+    @abstractmethod
+    def token_to_vector(self, token):
+        """Method obtains vector for given token.
+
+        Parameters
+        ----------
+        token : str
+            token from vocabulary
+
+        Returns
+        -------
+        vector : array_like
+            vector representation of given token
+
+        Raises
+        ------
+        KeyError
+            if given token doesn't have vector representation and default
+            vector function is not defined (None)
+        ValueError
+            if given token is None
+        RuntimeError
+            if vector storage is not initialized
+        """
+        pass
+
+    def __getitem__(self, key):
+        return self.token_to_vector(key)
+
+
+class BasicVectorStorage(VectorStorage):
+    """ Basic implementation of VectorStorage that handles loading vectors from
+    system storage.
+
+    Attributes
+    ----------
+    _vectors : dict
+        dictionary offering word to vector mapping
+    _dim : int
+        vector dimension
+    _initialized : bool
+        has the vector storage been initialized by loading vectors
+
+    """
+
+    def __init__(self, path, default_vector_function=zeros_default_vector,
+                 cache_path=None, max_vectors=None):
+        self._vectors = dict()
+        self._dim = None
+        self._initialized = False
+        super().__init__(
+            path=path,
+            default_vector_function=default_vector_function,
+            cache_path=cache_path,
+            max_vectors=max_vectors)
+
+    def load_all(self):
+        self._load_vectors()
+
+    def load_vocab(self, vocab):
+        if vocab is None:
+            raise ValueError("Vocab mustn't be None")
+        self._load_vectors(vocab=vocab)
+
+    def token_to_vector(self, token):
+        if not self._initialized:
+            raise RuntimeError("VectorStorage is not initialized."
+                               "Use load_all or load_vocab function"
+                               " to initialize.")
+        if token is None:
+            raise ValueError("Token mustn't be None")
+        if token not in self._vectors \
+           and self._default_vector_function is not None:
+            return self._default_vector_function(token, self._dim)
+        return self._vectors[token]
+
+    def _cache_vectors(self):
+        """Method for caching loaded vectors to cache_dir."""
+        with open(self._cache_path, "wb") as cache_file:
+            for word in self._vectors:
+                vector_values_string = " ".join(map(str, self._vectors[word]))
+                cache_file.write("{} {}\n".format(word, vector_values_string)
+                                 .encode('utf-8'))
+
+    @staticmethod
+    def _decode_word(word):
+        """Method tries to decode binary word as utf-8 raises UnicodeError if
+        fails.
+
+        Parameters
+        ----------
+        word : str
+            binary type string that needs to be decoded
+
+        Returns
+        -------
+        decoded : str
+            decoded word
+
+        Raises
+        ------
+        UnicodeDecodeError
+            if given word cannot be decoded in unicode
+        """
+        if isinstance(word, six.binary_type):
+            decoded = word.decode('utf-8')
+            return decoded
+
+    def _load_vectors(self, vocab=None):
+        """Internal method for loading vectors. It combines vocab vectors
+        loading and all vectors loading.
+
+        Parameters
+        ----------
+        vocab : iterable object
+            vocabulary with unique words
+
+        Raises
+        ------
+        UnicodeDecodeError
+            if given word cannot be decoded in unicode
+        RuntimeError
+            if file contains empty line or if it contains more that
+            one header line
+        ValueError
+            if given path is not a valid path or if the line in vector storage
+            cannot be casted to float
+        """
+        self._check_path()
+        curr_path = self._path if self._cache_path is None \
+            or not os.path.exists(self._cache_path) else self._cache_path
+
+        if vocab is not None and not isinstance(vocab, set):
+            vocab = set(vocab)
+
+        with open(curr_path, 'rb') as vector_file:
+
+            vectors_loaded = 0
+            header_lines = 0
+            for line in vector_file:
+                stripped_line = line.rstrip()
+                if not stripped_line:
+                    raise RuntimeError("File contains empty lines")
+
+                word, vector_entries_str = stripped_line.split(b" ", 1)
+                vector_entry = np.fromstring(string=vector_entries_str,
+                                             dtype=float, sep=' ')
+                # will throw ValueError if vector_entries_str cannot be casted
+
+                if self._dim is None and len(vector_entry) > 1:
+                    self._dim = len(vector_entry)
+                elif len(vector_entry) == 1:
+                    header_lines += 1
+                    if header_lines > 1:
+                        raise RuntimeError("Found more than one header line")
+                    continue  # probably a header, reference torch text
+                # second reference:
+                # https://radimrehurek.com/gensim/scripts/glove2word2vec.html
+                elif self._dim != len(vector_entry):
+                    raise RuntimeError(
+                        "Vector for token {} has {} dimensions, "
+                        "but previously read vectors have {} dimensions. "
+                        "All vectors must have the same "
+                        "number of dimensions.".format(word,
+                                                       len(vector_entry),
+                                                       self._dim))
+
+                word = self._decode_word(word)
+                if vocab is not None and word not in vocab:
+                    continue
+
+                self._vectors[word] = vector_entry
+                vectors_loaded += 1
+                if vectors_loaded == self._max_vectors:
+                    break
+
+            if self._cache_path is not None\
+               and not os.path.exists(self._cache_path):
+                self._cache_vectors()
+        self._initialized = True
+
+    def _check_path(self):
+        """Internal method for determining if instance paths are in supported
+        state. It enforces that both paths cannot be None and that not None
+        path must exist unless if it is used for caching loaded vectors.
+        """
+        if self._path is None and self._cache_path is None:
+            raise ValueError("Given vectors and cache paths mustn't"
+                             " be both None")
+
+        if self._path is not None and not os.path.exists(self._path):
+            raise ValueError("Given vectors path doesn't exist.")
+
+        if self._path is None and self._cache_path is not None\
+           and not os.path.exists(self._cache_path):
+            raise ValueError("Given cache path doesn't exist.")
---
+++ test/test_vectorizer.py
@@ -0,0 +1,310 @@
+import os
+import tempfile
+import pytest
+import numpy as np
+from takepod.storage import vectorizer
+
+BASIC_VECT_HEADING = b"251518 300"
+BASIC_VECT_DATA = [b". 0.001134 -0.000058 -0.000668\n",
+                   b"' -0.079721 -0.074874 -0.184826\n",
+                   b": -0.144256 -0.169637 -0.044801\n",
+                   b", -0.031469 -0.107289 -0.182500\n"]
+BASIC_VECT_DATA_DICT = {'.': np.array([0.001134, -0.000058, -0.000668]),
+                        "'": np.array([-0.079721, -0.074874, -0.184826]),
+                        ":": np.array([-0.144256, -0.169637, -0.044801]),
+                        ",": np.array([-0.031469, -0.107289, -0.182500])}
+DIFF_DIM_VECT_DATA = [b". 0.001134 -0.000058 -0.000668\n",
+                      b"' -0.079721 -0.074874\n",
+                      b", -0.031469 -0.107289 -0.182500\n"]
+
+
+def test_base_class_abstract():
+    with pytest.raises(TypeError):
+        vectorizer.VectorStorage(path="/")
+
+
+def test_basic_not_initialized():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path)
+    with pytest.raises(RuntimeError):
+        vect['.']
+    with pytest.raises(RuntimeError):
+        vect.token_to_vector('.')
+
+
+def test_basic_load_all_vectors():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path)
+    vect.load_all()
+    assert len(vect._vectors) == 4
+    assert vect['.'].shape == (3,)
+    assert vect.token_to_vector(',').shape == (3,)
+    assert np.allclose(a=vect['.'], b=BASIC_VECT_DATA_DICT['.'],
+                       rtol=0, atol=1.e-6)
+    assert np.allclose(a=vect.token_to_vector(','),
+                       b=BASIC_VECT_DATA_DICT[','],
+                       rtol=0, atol=1.e-6)
+
+
+def test_basic_load_with_header():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_header=BASIC_VECT_HEADING,
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path)
+    vect.load_all()
+    assert len(vect._vectors) == 4
+    assert vect['.'].shape == (3,)
+    assert vect.token_to_vector(',').shape == (3,)
+    assert np.allclose(a=vect['.'], b=BASIC_VECT_DATA_DICT['.'],
+                       rtol=0, atol=1.e-6)
+    assert np.allclose(a=vect.token_to_vector(','),
+                       b=BASIC_VECT_DATA_DICT[','],
+                       rtol=0, atol=1.e-6)
+
+
+def test_basic_no_token():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path,
+                                         default_vector_function=None)
+    vect.load_all()
+    with pytest.raises(KeyError):
+        print(vect['a'])
+    with pytest.raises(KeyError):
+        vect.token_to_vector('a')
+
+
+def test_basic_token_none():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path,
+                                         default_vector_function=None)
+    vect.load_all()
+    with pytest.raises(ValueError):
+        vect[None]
+    with pytest.raises(ValueError):
+        vect.token_to_vector(None)
+
+
+def test_basic_token_default():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(
+        path=vect_file_path,
+        default_vector_function=vectorizer.zeros_default_vector)
+    vect.load_all()
+    assert 'a' not in vect._vectors
+    assert vect['a'].shape == (3,)
+    assert np.allclose(a=vect.token_to_vector('a'),
+                       b=np.zeros(3),
+                       rtol=0, atol=1.e-6)
+
+
+def test_basic_load_vocab():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path,
+                                         default_vector_function=None)
+    vocab = ['.', ':']
+    vect.load_vocab(vocab=vocab)
+    assert len(vect._vectors) == 2
+    assert vect['.'].shape == (3,)
+    assert vect.token_to_vector(':').shape == (3,)
+    assert np.allclose(a=vect[':'], b=BASIC_VECT_DATA_DICT[':'],
+                       rtol=0, atol=1.e-6)
+    assert np.allclose(a=vect.token_to_vector('.'),
+                       b=BASIC_VECT_DATA_DICT['.'],
+                       rtol=0, atol=1.e-6)
+
+    with pytest.raises(KeyError):
+        vect[',']
+    with pytest.raises(KeyError):
+        vect.token_to_vector(',')
+
+
+def test_basic_load_vocab_none():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path)
+    with pytest.raises(ValueError):
+        vect.load_vocab(vocab=None)
+
+
+def test_basic_diff_dimensions():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=DIFF_DIM_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path)
+    with pytest.raises(RuntimeError):
+        vect.load_all()
+
+
+def test_default_vectors_zeros():
+    vector = vectorizer.zeros_default_vector(token="token", dim=5)
+    assert vector.shape == (5,)
+    assert np.allclose(a=vector, b=np.zeros(5), rtol=0, atol=1.e-6)
+
+
+def test_default_vectors_zeros_none_arguments():
+    with pytest.raises(ValueError):
+        vectorizer.zeros_default_vector(token="token", dim=None)
+
+    with pytest.raises(ValueError):
+        vectorizer.zeros_default_vector(token=None, dim=None)
+
+
+def test_basic_max_vectors_less_than_num_lines():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path, max_vectors=2)
+    vect.load_all()
+    assert len(vect._vectors) == 2
+    contained_elements = [".", "'"]
+    assert all(elem in vect._vectors for elem in contained_elements)
+    uncontained_elements = [":", ","]
+    assert all(elem not in vect._vectors for elem in uncontained_elements)
+
+
+def test_basic_max_vectors_vocab():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path, max_vectors=2)
+    vocab = ['.', ':', ","]
+    vect.load_vocab(vocab)
+    assert len(vect._vectors) == 2
+    contained_elements = [".", ":"]
+    assert all(elem in vect._vectors for elem in contained_elements)
+    uncontained_elements = ["'", ","]
+    assert all(elem not in vect._vectors for elem in uncontained_elements)
+
+
+def test_basic_max_vectors_bigger_than_num_lines():
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA)
+
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path, max_vectors=20)
+    vect.load_all()
+    assert len(vect._vectors) == 4
+    contained_elements = [".", "'", ":", ","]
+    assert all(elem in vect._vectors for elem in contained_elements)
+
+
+def test_basic_both_paths_none():
+    vect = vectorizer.BasicVectorStorage(path=None, cache_path=None)
+    with pytest.raises(ValueError):
+        vect.load_all()
+
+
+def test_basic_both_paths_doesnt_exist():
+    base = tempfile.mkdtemp()
+    assert os.path.exists(base)
+    file_path = os.path.join(base, 'file.t')
+    assert not os.path.exists(file_path)
+    cache_path = os.path.join(base, 'cache.t')
+    assert not os.path.exists(cache_path)
+
+    vect = vectorizer.BasicVectorStorage(path=file_path, cache_path=cache_path)
+    with pytest.raises(ValueError):
+        vect.load_all()
+
+
+def test_basic_path_none_cache_doesnt_exist():
+    base = tempfile.mkdtemp()
+    assert os.path.exists(base)
+    cache_path = os.path.join(base, 'cache.t')
+    assert not os.path.exists(cache_path)
+
+    vect = vectorizer.BasicVectorStorage(path=None, cache_path=cache_path)
+    with pytest.raises(ValueError):
+        vect.load_all()
+
+
+def test_basic_cache_max_vectors():
+    base = tempfile.mkdtemp()
+    assert os.path.exists(base)
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA,
+                                           base_dir=base)
+    assert os.path.exists(vect_file_path)
+    cache_path = os.path.join(base, 'cache.t')
+    assert not os.path.exists(cache_path)
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path,
+                                         max_vectors=2,
+                                         cache_path=cache_path)
+    vect.load_all()
+    assert os.path.exists(cache_path)
+    with open(cache_path, 'rb') as cache_file:
+        content = cache_file.readlines()
+        assert len(content) == 2
+        first_line_parts = content[0].split(b" ")
+        word, values = first_line_parts[0], first_line_parts[1:]
+        assert word == b"."
+        assert len(values) == 3
+
+
+def test_basic_cache_vocab():
+    base = tempfile.mkdtemp()
+    assert os.path.exists(base)
+    vect_file_path = create_temp_vect_file(vect_file_name="vect1",
+                                           file_data=BASIC_VECT_DATA,
+                                           base_dir=base)
+    assert os.path.exists(vect_file_path)
+    cache_path = os.path.join(base, 'cache.t')
+    assert not os.path.exists(cache_path)
+    vect = vectorizer.BasicVectorStorage(path=vect_file_path,
+                                         cache_path=cache_path)
+
+    vocab = ['.', ':', ","]
+    vect.load_vocab(vocab)
+
+    assert os.path.exists(cache_path)
+
+    with open(cache_path, 'rb') as cache_file:
+        content = cache_file.readlines()
+        assert len(content) == 3
+
+
+def create_temp_vect_file(vect_file_name, file_data,
+                          file_header=None, base_dir=None):
+    """Helper function that creates temporary vector file with given data.
+
+    Parameters
+    ----------
+    vect_file_name : str
+        temporary vector file file name
+    file_data : byte str
+        list of lines to be written
+    file_header : byte str, optional
+        header text, if None nothing is written
+    base_dir : str
+        path to base directory where to create vector file
+
+    Returns
+    -------
+    vect_file_path : str
+        path to the created vector file
+    """
+    if base_dir is None:
+        base_dir = tempfile.mkdtemp()
+        assert os.path.exists(base_dir)
+
+    file_path = os.path.join(base_dir, vect_file_name)
+
+    with open(file_path, 'wb') as vect_file:
+        if file_header is not None:
+            vect_file.write(file_header+b"\n")
+        vect_file.writelines(file_data)
+
+    assert os.path.exists(file_path)
+    return file_path
