76d283073dab31c7f7f0509078d1321af5f5b460
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2018-10-31 14:49:42 +0100

    vocab definition
    from vocab branch

---
+++ takepod/storage/vocab.py
@@ -0,0 +1,283 @@
+"""Module contains classes related to the vocabulary."""
+from collections import Counter, defaultdict
+from enum import Enum
+import numpy as np
+
+
+class SpecialVocabSymbols(Enum):
+    """Enumeration of special vocabular symbols
+
+    Attributes
+    ----------
+    UNK : str
+        Tag for unknown word
+    PAD : str
+        TAG for padding symbol
+    """
+    UNK = "<unk>"
+    PAD = "<pad>"
+
+
+class Vocab:
+    """Class for storing vocabulary. It supports frequency counting and size
+    limiting.
+
+    Attributes
+    ----------
+    finalized : bool
+        true if the vocab is finalized, false otherwise
+    itos : list
+        list of words
+    stoi : dict
+        mapping from word string to index
+    """
+    def __init__(self, max_size=None, min_freq=1,
+                 specials=(SpecialVocabSymbols.UNK, SpecialVocabSymbols.PAD),
+                 keep_freqs=False, stop_words=None):
+        """Vocab constructor. Specials are first in the vocabulary.
+
+        Parameters
+        ----------
+        max_size : int
+            maximal vocab size
+        min_freq : int
+            words with frequency lower than this will be removed
+        specials : iter(str)
+            collection of special symbols
+        keep_freqs : bool
+            if true word frequencies will be saved for later use on
+            the finalization
+        stop_words : set(str), optional
+            set of stop words
+        """
+        self._freqs = Counter()
+        self._keep_freqs = keep_freqs
+        self._min_freq = min_freq
+
+        self.itos = list(specials)
+        self._default_unk_index = self._init_default_unk_index(specials)
+        self.stoi = defaultdict(self._default_unk)
+        self.stoi.update({k: v for v, k in enumerate(self.itos)})
+
+        self._max_size = max_size
+        self._stop_words = stop_words
+        self.finalized = False   # flag to know if we're ready to numericalize
+
+    @staticmethod
+    def _init_default_unk_index(specials):
+        """Method computes index of default unknown symbol in given collection.
+
+        Parameters
+        ----------
+        specials : iter(SpecialVocabSymbols)
+            collection of special vocab symbols
+
+        Returns
+        -------
+        index : int or None
+            index of default unkwnown symbol or None if it doesn't exist
+        """
+        ind = 0
+        for spec in specials:
+            if spec == SpecialVocabSymbols.UNK:
+                return ind
+            ind += 1
+        return None
+
+    def _default_unk(self):
+        """Method obtains default unknown symbol index. Used for stoi.
+
+        Returns
+        -------
+        index: int
+            index of default unknown symbol
+
+        Raises
+        ------
+        ValueError
+            if unknown symbol is not present in the vocab
+        """
+        if self._default_unk_index is None:
+            raise ValueError("Unknown symbol is not present in the vocab.")
+        return self._default_unk_index
+
+    def get_freqs(self):
+        """Method obtains vocabulary frequencies.
+
+        Returns
+        -------
+        freq : Counter
+            mapping frequency for every word
+
+        Raises
+        ------
+        ValueError
+            if the user stated that he doesn't want to keep frequencies
+        """
+        if not self._keep_freqs:
+            raise ValueError("User specified that the frequencies "
+                             "are not kept")
+        return self._freqs
+
+    @property
+    def pad_symbol(self):
+        """Method returns padding symbol index.
+
+        Returns
+        -------
+        pad_symbol_index : int
+            padding symbol index in the vocabullary
+
+        Raises
+        ------
+        ValueError
+            if the padding symbol is not pressent in the vocabulary
+        """
+        if SpecialVocabSymbols.PAD not in self.stoi:
+            raise ValueError("Padding symbol is not in the vocabulary")
+        return self.stoi[SpecialVocabSymbols.PAD]
+
+    def __add__(self, values):
+        """Method allows a vocabulary to be added to current vocabulary or
+        that a set of values is added to the vocabulary.
+
+        Parameters
+        ----------
+        values : set or Vocab
+            values to be added to the vocabulary
+
+        Returns
+        -------
+        vocab : Vocab
+            returns current Vocab instance to enable chaining
+
+        Raises
+        ------
+        RuntimeError
+            if the current vocab is finalized
+        """
+        if self.finalized:
+            raise RuntimeError("Finalized vocabulary cannot be changed.")
+
+        if isinstance(values, Vocab):
+            self._freqs += values.freqs  # add freqs to this instance
+        else:
+            try:
+                self._freqs.update(values)
+            except TypeError:
+                raise TypeError("Vocab supports only adding vocab or set"
+                                "to vocab")
+        return self
+
+    def __iadd__(self, values):
+        """Method allows a vocabulary to be added to current vocabulary or
+        that a set of values is added to the vocabulary.
+
+        Parameters
+        ----------
+        values : set or Vocab
+            values to be added to the vocabulary
+
+        Returns
+        -------
+        vocab : Vocab
+            returns current Vocab instance to enable chaining
+
+        Raises
+        ------
+        RuntimeError
+            if the current vocab is finalized
+        """
+        return self.__add__(values)
+
+    def finalize(self):
+        """Method finalizes vocab building. It also releases frequency counter
+        if user set not to keep them.
+
+        Raises
+        ------
+        RuntimeError
+            if the vocab is already finalized
+        """
+        if self.finalized:
+            raise RuntimeError("Vocab is already finalized.")
+
+        # construct stoi and itos, sort by frequency
+        words_and_freqs = sorted(self._freqs.items(), key=lambda tup: tup[1],
+                                 reverse=True)
+        if self._max_size is None:
+            self._max_size = len(words_and_freqs) + len(self.itos)
+            # vocab + specials
+        for word, freq in words_and_freqs:
+            if freq < self._min_freq or len(self.itos) >= self._max_size:
+                break
+            if word in self._stop_words:
+                continue
+            self.itos.append(word)
+            self.stoi[word] = len(self.stoi)
+
+        if not self._keep_freqs:
+            self._freqs = None  # release memory
+
+        self.finalized = True
+
+    def numericalize(self, data):
+        """Method numericalizes given tokens.
+
+        Parameters
+        ----------
+        data : iter(str)
+            iterable collection of tokens
+
+        Returns
+        -------
+        numericalized_vector : array-like
+            numpy array of numericalized tokens
+
+        Raises
+        ------
+        ValueError
+            if the vocabulary is not finalized
+        """
+        if not self.finalized:
+            raise ValueError('Cannot numericalize if the vocabulary has not'
+                             ' been finalized call `.finalize() on the Field`')
+        return np.array([self.stoi[token] for token in data])
+
+    def __len__(self):
+        """Method calculates vocab lengths including special symbols.
+
+        Returns
+        -------
+        length : int
+            vocab size including special symbols
+        """
+        if self.finalized:
+            return len(self.itos)
+        return len(self._freqs)
+
+    def __eq__(self, other):
+        """Two vocabs are same if they have same finalization status, their
+        stoi and itos mappings are same and their frequency counters are same.
+
+        Parameter
+        ---------
+        other : object
+            object for which we want to knwo equality propertiy
+
+        Returns
+        -------
+        equal : bool
+            true if two vocabs are same, false otherwise
+        """
+        if not isinstance(other, Vocab):
+            return False
+        if self.finalized != other.finalized:
+            return False
+        if self._freqs != other.freqs:
+            return False
+        if self.stoi != other.stoi:
+            return False
+        if self.itos != other.itos:
+            return False
+        return True
