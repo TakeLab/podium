bdda3481a46f78f7dc8fe4219c94ef70645694bd
Mate Mijolovic <mate.mijolovic@ingemark.com>
2018-12-14 16:59:39 +0100

    Implement loader for croatian NER dataset

---
+++ takepod/dataload/ner_croatian.py
@@ -0,0 +1,196 @@
+"""Simple NERCroatian dataset module."""
+import glob
+import os
+import xml.etree.ElementTree as ET
+
+
+class NERCroatian:
+    """Simple croatian NER class"""
+
+    SENTENCE_DELIMITER_TOKEN = (None, None)
+
+    def __init__(self,
+                 path='downloaded_datasets/',
+                 tokenizer='split',
+                 tag_schema='BIO'):
+        """Constructor for Croatian NER dataset
+
+        Parameters
+        ----------
+        path: str
+            Path to the folder where the dataset should be downloaded or loaded
+            from if it is already downloaded
+        tokenizer: str
+            Word-level tokenizer used to tokenize the input text
+            - supported tokenizers:
+                - 'split': simple tokenizer that splits the sentence on
+                    whitespaces (using str.split)
+        tag_schema: str
+            Tag schema used when converting the dataset to TSV format
+            - supported tag schemas:
+                - 'BIO': the label of the beginning token of the entity is
+                prefixed with 'B-', the remaining tokens that belong to the
+                same entity are prefixed with 'I-'. The tokens that don't
+                belong to any named entity are labeled 'O'
+        """
+        self._data_dir = path
+        self._tokenizer = self._get_tokenizer(tokenizer)
+        self._label_resolver = self._get_label_resolver(tag_schema)
+
+    def load_dataset(self):
+        """
+        Method loads the dataset and returns tokenized NER documents.
+
+        Returns
+        -------
+        tokenized_documents: list of lists of tuples
+            List of tokenized documents. Each document is represented
+            as a list of tuples (token, label). The sentences in document are
+            delimited by tuple (None, None)
+        """
+        source_dir_location = os.path.join(self._data_dir, 'croatian_ner',
+                                           'CroatianNERDataset')
+
+        tokenized_documents = []
+
+        for xml_file_path in glob.glob(source_dir_location + '/*.xml'):
+            word_label_pairs = self._xml_to_token_label_pairs(xml_file_path)
+            tokenized_documents.append(word_label_pairs)
+
+        return tokenized_documents
+
+    def _xml_to_token_label_pairs(self, xml_file_path):
+        """
+        Converts the xml file located at the given path to the list of tuples
+        (token, label)
+
+        Parameters
+        ----------
+        xml_file_path: str
+            Path to the XML file
+
+        Returns
+        -------
+        token_label_pairs: list of tuples
+            List of tuples (token, label)
+        """
+        root = ET.parse(xml_file_path).getroot()
+
+        token_label_pairs = []
+
+        for sentence in root.iter(tag='s'):
+            for sub_element in sentence.iter():
+
+                if sub_element.text is not None:
+                    token_label_pairs_subelement = \
+                        self._tokenize(sub_element.text.strip(), sub_element)
+                    token_label_pairs.extend(token_label_pairs_subelement)
+
+                if sub_element.tail is not None:
+                    token_label_pairs_outside = \
+                        self._tokenize(sub_element.tail.strip())
+                    token_label_pairs.extend(token_label_pairs_outside)
+
+            token_label_pairs.append(self.SENTENCE_DELIMITER_TOKEN)
+
+        return token_label_pairs
+
+    def _tokenize(self, text, element=None):
+        """
+        Method tokenizes the text and assigns it the label according to
+        the element's 'type' attribute.
+
+        Parameters
+        ----------
+        text: str
+            Input text
+        element: ET.Element
+            Element with which the text is associated. If set to None, the
+            default label 'O' is set.
+
+        Returns
+        -------
+        token_label_pairs: list of tuples
+            List of tuples (token, label)
+        """
+        if len(text) == 0:
+            return []
+
+        tokenized_text = self._tokenizer(text)
+
+        token_label_pairs = []
+        for index, token in enumerate(tokenized_text):
+            if element is not None:
+                label_unprefixed = element.attrib.get('type', 'O')
+                label = self._label_resolver(index, label_unprefixed)
+            else:
+                label = 'O'
+            token_label_pairs.append((token, label))
+
+        return token_label_pairs
+
+    def _get_tokenizer(self, tokenizer_name):
+        """
+        Gets the tokenizer implementation associated with the given
+        tokenizer name
+
+        Parameters
+        ----------
+        tokenizer_name: str
+            Name of the tokenizer
+
+        Returns
+        -------
+        tokenizer: callable
+            Tokenizer instance associated with the given tokenizer name
+        """
+        if tokenizer_name == 'split':
+            return str.split
+
+        raise ValueError('Unknown tokenizer specified: {}'
+                         .format(tokenizer_name))
+
+    def _get_label_resolver(self, tag_schema):
+        """
+        Gets the label resolver associated with the given tag schema
+
+        Parameters
+        ----------
+        tag_schema: str
+            Tag schema for label prefixes
+
+        Returns
+        -------
+        label_resolver: callable
+            Label resolver associated with the given tag schema
+        """
+        if tag_schema == 'BIO':
+            return self._bio_label_resolver
+
+        raise ValueError('No label resolver for tag schema {} exists'
+                         .format(tag_schema))
+
+    @staticmethod
+    def _bio_label_resolver(index, label):
+        """
+        A resolver that prefixes the label according to the BIO tag schema.
+
+        Parameters
+        ----------
+        index: int
+            Index of the token/label in the named entity (starts from 0)
+        label: str
+            Label of the named entity
+
+        Returns
+        -------
+            Label prefixed with the appropriate prefix according to the BIO
+            tag schema
+        """
+        if label == 'O':
+            return label
+
+        if index == 0:
+            return 'B-' + label
+        else:
+            return 'I-' + label
---
+++ test/test_ner_croatian.py
@@ -0,0 +1,112 @@
+from takepod.dataload.ner_croatian import NERCroatian
+import xml.etree.ElementTree as ET
+import tempfile
+import pytest
+import shutil
+import os
+
+
+def create_ner_file(filepath, title_element, body_element):
+    root = ET.Element("root")
+
+    root.append(title_element)
+    root.append(body_element)
+
+    tree = ET.ElementTree(root)
+    tree.write(filepath, encoding='utf-8')
+
+
+title_1 = ET.fromstring("""
+<title>
+    <s>Random <enamex type="Organization">Entitet</enamex></s>
+</title>
+""")
+
+body_1 = ET.fromstring("<body><s>Ovdje nema entiteta!</s></body>")
+
+expected_output_1 = [
+    ('Random', 'O'),
+    ('Entitet', 'B-Organization'),
+    NERCroatian.SENTENCE_DELIMITER_TOKEN,
+    ('Ovdje', 'O'),
+    ('nema', 'O'),
+    ('entiteta!', 'O'),
+    NERCroatian.SENTENCE_DELIMITER_TOKEN
+]
+
+
+title_2 = ET.fromstring("""
+<title>
+    <s>
+        <enamex type="LocationAsOrganization">Kina</enamex>
+        je najveći svjetski izvoznik
+    </s>
+</title>
+""")
+
+body_2 = ET.fromstring("""
+<body>
+    <s>
+        Ukupna vrijednost izvoza <timex type="Date">u prvoj polovini
+        ove godine</timex> iznosila je <numex type="Money">521,7
+        milijardi dolara</numex>.
+    </s>
+</body>
+""")
+
+expected_output_2 = [
+    ('Kina', 'B-LocationAsOrganization'),
+    ('je', 'O'),
+    ('najveći', 'O'),
+    ('svjetski', 'O'),
+    ('izvoznik', 'O'),
+    NERCroatian.SENTENCE_DELIMITER_TOKEN,
+    ('Ukupna', 'O'),
+    ('vrijednost', 'O'),
+    ('izvoza', 'O'),
+    ('u', 'B-Date'),
+    ('prvoj', 'I-Date'),
+    ('polovini', 'I-Date'),
+    ('ove', 'I-Date'),
+    ('godine', 'I-Date'),
+    ('iznosila', 'O'),
+    ('je', 'O'),
+    ('521,7', 'B-Money'),
+    ('milijardi', 'I-Money'),
+    ('dolara', 'I-Money'),
+    ('.', 'O'),
+    NERCroatian.SENTENCE_DELIMITER_TOKEN
+]
+
+
+@pytest.mark.parametrize(
+    "expected_data, expected_output",
+    [
+        ((title_1, body_1), expected_output_1),
+        ((title_2, body_2), expected_output_2),
+    ]
+)
+def test_load_dataset(tmpdir, expected_data, expected_output):
+    base = tempfile.mkdtemp()
+    assert os.path.exists(base)
+
+    unzipped_xml_directory = os.path.join(base, 'croatian_ner',
+                                          'CroatianNERDataset')
+
+    os.makedirs(unzipped_xml_directory)
+
+    title, body = expected_data
+    create_ner_file(
+        os.path.join(unzipped_xml_directory, "file.xml"),
+        title,
+        body
+    )
+
+    ner_croatian_ds = NERCroatian(base, tokenizer='split', tag_schema='BIO')
+    documents = ner_croatian_ds.load_dataset()
+
+    assert len(documents) == 1
+    assert documents[0] == expected_output
+
+    shutil.rmtree(base)
+    assert not os.path.exists(base)
