9626a417a77c8798cce7040e79fc29ce26228354
Domagoj Pluščec <domagoj.pluscec@fer.hr>
2019-01-04 14:33:31 +0100

    base model and trainer docs

--- takepod/models/base_model.py
+++ takepod/models/base_model.py
@@ -1,20 +1,96 @@
+"""Module contains base model interfaces."""
 from abc import ABC, abstractmethod
 
 
 class AbstractSupervisedModel(ABC):
-    
+    """Interface for supervised models."""
+    ## TODO (dpluscec) add some general keyword argument types
     @abstractmethod
-    def fit(X, y, **kwargs):
-        pass
-    
-    @abstractmethod
-    def predict(X, **kwargs):
+    def fit(self, X, y, **kwargs):
+        """Method trains the model and returns dictionary of values defined by
+        model specific key parameters
+
+        Parameters
+        ----------
+        X : np.array
+            input data
+        y : np.array
+            data labels
+        **kwargs : dict
+            Additional key-value parameters for model
+
+        Returns
+        -------
+        result : dict
+            dictionary mapping fit results to defined model specific key
+            parameters
+        """
         pass
 
-class AbstractFrameworkModel(ABC):
     @abstractmethod
-    def save(**kwargs):
+    def predict(self, X, **kwargs):
+        """Predict labels for given data
+
+        Parameters
+        ----------
+        X : np.array
+            input data
+        **kwargs : dict
+            Additional key-value parameters for model
+
+        Returns
+        -------
+        result : dict
+            dictionary mapping fit results to defined model specific key
+            parameters
+        """
         pass
+
+
+class AbstractFrameworkModel(ABC):
+    """Interface for framework models."""
+
     @abstractmethod
-    def load(**kwargs):
+    def save(self, file_path, **kwargs):
+        """Method saves model to given file_path with additional arguments
+        defined in kwargs.
+
+        Parameters
+        ----------
+        file_path : str
+            path to file where the model should be saved
+        **kwargs : dict
+            Additional key-value parameters for saving mode
+
+        Raises
+        ------
+        IOError
+            if there was an error while writing to a file
+        """
+        pass
+
+    @abstractmethod
+    def load(self, **kwargs):
+        """Method loads model from given file_path with additional arguments
+        defined in kwargs.
+
+        Parameters
+        ----------
+        file_path : str
+            path to file where the model should be saved
+        **kwargs : dict
+            Additional key-value parameters for loading model
+
+        Returns
+        -------
+        model
+            method returns loaded model
+
+        Raises
+        ------
+        ValueError
+            if the given path doesn't exist
+        IOError
+            if there was an error while reading from a file
+        """
         pass
--- takepod/models/base_trainer.py
+++ takepod/models/base_trainer.py
@@ -1,6 +1,19 @@
+"""Module contains interfaces for a trainer."""
 from abc import ABC, abstractmethod
 
+
 class AbstractTrainer(ABC):
+    """Interface for base trainer that can train the model."""
+
     @abstractmethod
-    def train (iterator):
+    def train(self, iterator, **kwargs):
+        """Method trains a model with data from given iterator.
+
+        Parameters
+        ----------
+        iterator : Iterator
+            iterator instance that provides data from a dataset
+        kwargs : dict
+            trainer specific parameters
+        """
         pass
