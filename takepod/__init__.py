da1aba8fae192f4b5fa64384c1aafc2884350156
mttk <martin.tutek@gmail.com>
2019-02-08 16:24:28 +0100

    reorder imports

--- takepod/__init__.py
+++ takepod/__init__.py
@@ -1,1 +1,18 @@
-name = "takepod"
+from . import dataload
+from . import datasets
+from . import examples
+from . import metrics
+from . import models
+from . import preproc
+from . import storage
+
+__name__ = "takepod"
+__version__ = "0.1.0"
+
+__all__ = ["dataload",
+           "datasets",
+           "examples",
+           "metrics",
+           "models",
+           "preproc", 
+           "storage"]
--- takepod/storage/__init__.py
+++ takepod/storage/__init__.py
@@ -1,1 +1,28 @@
 """Package contains modules for storing and loading datasets and vectors."""
+
+from .dataset import Dataset, TabularDataset, stratified_split, rationed_split
+from .downloader import BaseDownloader, SCPDownloader, HttpDownloader, SimpleHttpDownloader
+from .example import Example
+from .field import Field
+from .iterator import Iterator, BucketIterator
+from .large_resource import LargeResource, SCPLargeResource
+from .vectorizer import VectorStorage, BasicVectorStorage
+from .vocab import SpecialVocabSymbols, Vocab
+
+# Convention: class imports from same module are continuous in one line until the length
+# limit, when they continue in the next line. Imports from subsequent modules are broken
+# with newlines. 
+# The order of imports is the same as the alphabetical order of files in the current
+# module, as well as the ordering in `__all__`.
+# Function imports follow the same convention, but behind class imports.
+
+
+__all__ = ["Dataset", "TabularDataset",
+           "BaseDownloader", "SCPDownloader", "HttpDownloader", "SimpleHttpDownloader",
+           "Example",
+           "Field",
+           "Iterator", "BucketIterator",
+           "LargeResource", "SCPLargeResource",
+           "VectorStorage", "BasicVectorStorage",
+           "SpecialVocabSymbols", "Vocab",
+           "stratified_split", "rationed_split"]
