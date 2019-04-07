655e3acf00d2b55961c09955f950509937d29468
Domagoj Pluscec <domagoj.pluscec@fer.hr>
2019-04-07 13:39:26 +0200

    Imdb dataset (#79)
    * tar file fix
    * imdb dataset
    * fixes to pauza dataset
    
    ```
    ----------- coverage: platform win32, python 3.6.2-final-0 -----------
    Name                                                Stmts   Miss  Cover   Missing
    ---------------------------------------------------------------------------------
    takepod\__init__.py                                    15      0   100%
    takepod\dataload\__init__.py                            0      0   100%
    takepod\dataload\ner_croatian.py                       69      0   100%
    takepod\datasets\__init__.py                            3      0   100%
    takepod\datasets\catacx_comments_dataset.py            40      4    90%   54-67
    takepod\datasets\catacx_dataset.py                     55      3    95%   28-29, 48
    takepod\datasets\croatian_ner_dataset.py               37      0   100%
    takepod\datasets\imdb_sentiment_dataset.py             48      0   100%
    takepod\datasets\pauza_dataset.py                      36      0   100%
    takepod\examples\__init__.py                            0      0   100%
    takepod\examples\dataset_example.py                    14     14     0%   2-28
    takepod\examples\model_example.py                      49     21    57%   62-78, 85-87, 95-108, 114-115
    takepod\examples\vectors_example.py                    15     15     0%   3-25
    takepod\metrics\__init__.py                             2      0   100%
    takepod\metrics\metrics.py                              3      0   100%
    takepod\models\__init__.py                              2      0   100%
    takepod\models\base_model.py                           12      4    67%   36, 55, 78, 104
    takepod\models\base_trainer.py                          6      1    83%   29
    takepod\models\fc_model.py                             11      0   100%
    takepod\models\simple_trainers.py                      17      0   100%
    takepod\models\svm_model.py                            10      0   100%
    takepod\preproc\__init__.py                             4      0   100%
    takepod\preproc\lemmatizer\__init__.py                  2      0   100%
    takepod\preproc\lemmatizer\croatian_lemmatizer.py      65      1    98%   197
    takepod\preproc\stemmer\__init__.py                     2      0   100%
    takepod\preproc\stemmer\croatian_stemmer.py            46      0   100%
    takepod\preproc\stop_words.py                          13      0   100%
    takepod\preproc\tokenizers.py                          21      0   100%
    takepod\preproc\util.py                                35      0   100%
    takepod\storage\__init__.py                             9      0   100%
    takepod\storage\dataset.py                            294      7    98%   849, 885-888, 948-951
    takepod\storage\downloader.py                          54     15    72%   45, 113-132, 160
    takepod\storage\example.py                             58      0   100%
    takepod\storage\field.py                              132      3    98%   410-412
    takepod\storage\iterator.py                           159     13    92%   473-478, 481-485, 523, 571, 579, 600-604, 607
    takepod\storage\large_resource.py                      59      1    98%   109
    takepod\storage\utility.py                             28      9    68%   53-55, 77-82
    takepod\storage\vectorizer.py                         151     17    89%   86, 109, 135, 154, 303-306, 318-322, 476-488
    takepod\storage\vocab.py                              110      6    95%   232-235, 318, 322, 324, 326
    ---------------------------------------------------------------------------------
    TOTAL                                                1686    134    92%
    
    
    ========================= 348 passed in 8.32 seconds ================
    ```

--- takepod/datasets/pauza_dataset.py
+++ takepod/datasets/pauza_dataset.py
@@ -1,10 +1,6 @@
 """Module contains PauzaHR datasets."""
 import os
-from takepod.storage import dataset
-from takepod.storage.example import Example
-from takepod.storage.field import Field
-from takepod.storage.vocab import Vocab
-from takepod.storage.large_resource import LargeResource
+from takepod.storage import (dataset, Example, Field, Vocab, LargeResource)
 
 
 class PauzaHRDataset(dataset.Dataset):
@@ -18,6 +14,8 @@ class PauzaHRDataset(dataset.Dataset):
         dataset name
     DATASET_DIR : str
         name of the folder in the dataset containing train and test directories
+    ARCHIVE_TYPE : str
+        string that defines archive type, used for unpacking dataset
     TRAIN_DIR : str
         name of the training directory
     TEST_DIR : str
@@ -87,7 +85,7 @@ class PauzaHRDataset(dataset.Dataset):
         ----------
         fields : dict(str, Field), optional
             dictionary mapping field name to field, if not given method will
-            use ```_get_default_fields```.
+            use ```get_default_fields```.
 
         Returns
         -------
@@ -97,7 +95,7 @@ class PauzaHRDataset(dataset.Dataset):
         data_location = os.path.join(LargeResource.BASE_RESOURCE_DIR,
                                      PauzaHRDataset.DATASET_DIR)
         if not fields:
-            fields = PauzaHRDataset._get_default_fields()
+            fields = PauzaHRDataset.get_default_fields()
 
         train_dataset = PauzaHRDataset(dir_path=os.path.join(
             data_location, PauzaHRDataset.TRAIN_DIR), fields=fields)
@@ -108,7 +106,7 @@ class PauzaHRDataset(dataset.Dataset):
         return (train_dataset, test_dataset)
 
     @staticmethod
-    def _get_default_fields():
+    def get_default_fields():
         """Method returns default PauzaHR fields: rating, source and text.
 
         Returns
