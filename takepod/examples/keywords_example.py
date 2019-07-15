b00f0c9217c4cc65253ed2d438218da341202e12
Antonio Šajatović <antonio.sajatovic@gmail.com>
2019-07-15 15:26:48 +0200

    Implements YAKE

---
+++ takepod/examples/keywords_example.py
@@ -0,0 +1,75 @@
+from takepod.storage import (dataset, Field, ExampleFactory)
+from takepod.storage.yake import YAKE
+
+
+class KWDummyDataset(dataset.Dataset):
+    """Dummmy Keyword Extraction dataset. Consists of a single example (document)
+       with a single field (raw text).
+
+    Attributes
+    ----------
+    TEXT_FIELD_NAME : str
+        name of the field containing the document text
+    """
+
+    TEXT_FIELD_NAME = "text"
+
+    def __init__(self, text):
+        """
+        Dataset constructor.
+
+        Parameters
+        ----------
+        text : str
+            single document text contents
+        """
+        fields = KWDummyDataset.get_default_fields()
+        unpacked_fields = dataset.unpack_fields(fields=fields)
+        example_factory = ExampleFactory(fields)
+        examples = [example_factory.from_dict({"text": text})]
+        super(KWDummyDataset, self).__init__(
+            **{"examples": examples, "fields": unpacked_fields})
+
+    @staticmethod
+    def get_default_fields():
+        """Method returns default keyword extraction fields: text.
+
+        Returns
+        -------
+        fields : dict(str, Field)
+            Dictionary mapping field name to field.
+        """
+        text = Field(KWDummyDataset.TEXT_FIELD_NAME,
+                     tokenizer='split',
+                     language='en',
+                     vocab=None,
+                     tokenize=False,
+                     store_as_raw=True,
+                     store_as_tokenized=False
+                     )
+        return {KWDummyDataset.TEXT_FIELD_NAME: text}
+
+
+def keyword_extraction_main(lang):
+    texts = {"en": """Sources tell us that Google is acquiring Kaggle, a platform that hosts
+                      data science and machine learning competitions. Details about the
+                      transaction remain somewhat vague, but given that Google is hosting
+                      its Cloud Next conference in San Francisco this week, the official
+                      announcement could come as early as tomorrow.  Reached by phone,
+                      Kaggle co-founder CEO Anthony Goldbloom declined to deny that the
+                      acquisition is happening. Google itself declined
+                      'to comment on rumors'.
+                   """,
+             "hr": """kljucna rijec"""}
+
+    dataset = KWDummyDataset(texts[lang])
+    kw_extractor = YAKE(lang)
+    keywords = kw_extractor.transform(dataset[0])
+    return keywords
+
+
+if __name__ == "__main__":
+    keywords_en = keyword_extraction_main("en")
+    print(*keywords_en, sep='\n')
+    # keywords_hr = keyword_extraction_main("hr")
+    # print(*keywords_hr, sep='\n')
---
+++ takepod/storage/yake.py
@@ -0,0 +1,58 @@
+import yake
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class YAKE():
+    """Yet Another Keyword Extractor is and unsupervised, corpus-independent, and
+       domain and language independent keyword extraction algorithm for extraction from
+       single documents. This class is a wrapper of the official implementation available
+       at https://github.com/LIAAD/yake."""
+
+    def __init__(self, lang="en", ngram_size=3, dedup_lim=0.9, dedup_func='seqm',
+                 windows_size=1, top=20):
+        """Constructor that initializes YAKE.
+
+        Parameters
+        ----------
+        lang : str
+            the language of the input text. If None, defaults to English.
+        ngram_size: int
+            maximum ngram size
+        dedup_lim: float
+            deduplication limit, see yake documentation for details
+        dedup_func: str
+            deduplication function, see yake documentation for details
+        windows_size: int
+            context window size
+        top: int
+            max number of keyphrases to extract
+        """
+        self._kw_extractor = yake.KeywordExtractor(lan=lang,
+                                                   n=ngram_size,
+                                                   dedupLim=dedup_lim,
+                                                   dedupFunc=dedup_func,
+                                                   windowsSize=windows_size,
+                                                   top=top,
+                                                   features=None)
+
+    def transform(self, example):
+        """Extracts keywords from the example.
+
+        Parameters
+        ----------
+        example : Example
+            dataset example containing a text field
+        Returns
+        -------
+        keywords : list of (str, float)
+            list of (keyword, score) sorted ascending by score
+        """
+        if example is None:
+            error_msg = "examples mustn't be None"
+            _LOGGER.error(error_msg)
+            raise ValueError(error_msg)
+        raw_text = example.text[0]
+        keywords = self._kw_extractor.extract_keywords(raw_text)
+        return keywords
