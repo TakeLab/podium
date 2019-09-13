4b39de45e154f62b3e528a435a52c9b532bdf7ab
Ivan Smoković <ivasmo95@gmail.com>
2019-09-13 16:13:08 +0200

    Added documentation to grid_search

--- takepod/model_selection/__init__.py
+++ takepod/model_selection/__init__.py
@@ -1,3 +1,4 @@
+"""This package contains model selection methods."""
 from .grid_search import grid_search
 
 __all__ = ["grid_search"]
--- takepod/model_selection/grid_search.py
+++ takepod/model_selection/grid_search.py
@@ -1,7 +1,7 @@
 from typing import Callable, Iterable, Tuple, Dict, Any, Union
 
 import numpy as np
-from sklearn.model_selection import ParameterGrid
+from sklearn.model_selection import ParameterGrid, GridSearchCV
 import logging
 from tqdm import tqdm
 
@@ -21,6 +21,54 @@ def grid_search(
         n_splits: int = 5,
         greater_is_better: bool = True,
         print_progress: bool = True) -> Tuple[float, Dict[str, Any], Dict[str, Any]]:
+    """Method used to find the best combination of training and model hyperparameters
+    out of the given hyperparameters. This method uses simple grid search to evaluate
+    all possible combinations of the given hyperparameters. Based on sklearn's
+    :class:`GridSearchCV`. Each hyperparameter combination is scored by first training
+    the model and then evaluating that model using k-fold cross validation. The final
+    score for a set of hyperparameters is the mean of scores across all folds.
+
+
+    Parameters
+    ----------
+    experiment : Experiment
+        Experiment defining the training and prediction procedure to be optimised.
+
+    dataset : Dataset
+        Dataset to be used in the hyperparameter search.
+
+    score_fun : callable
+        Function used to score a hyperparameter set.
+
+    model_param_grid : Dict or Iterable of Dicts
+        The model parameter grid. Combinations taken from this grid are passed to the
+        model's __init__ function. Dictionary with parameters names (string) as keys and
+        lists of parameter settings to try as values, or a list of such dictionaries,
+        in which case the grids spanned by each dictionary in the list are explored.
+        This enables searching over any sequence of parameter settings.
+
+    trainer_param_grid : Dict or Iterable of Dicts
+        The trainer parameter grid. Combinations taken from this grid are passed to the
+        trainers's train function. Dictionary with parameters names (string) as keys and
+        lists of parameter settings to try as values, or a list of such dictionaries,
+        in which case the grids spanned by each dictionary in the list are explored.
+        This enables searching over any sequence of parameter settings.
+
+    n_splits : int
+        Number of folds to be used in cross-validation.
+
+    greater_is_better : bool
+        Whether score_func is a score function (default), meaning high is good, or a loss
+        function, meaning low is good.
+
+    print_progress : bool
+        Whether to print progress. Progress is printed to sys.stderr.
+
+
+    See Also
+    --------
+        :class:`GridSearchCV`
+    """
     # TODO: Add dataset shuffling to cross-validation
 
     best_model_params = None
@@ -48,18 +96,22 @@ def grid_search(
                                       dataset,
                                       n_splits=n_splits,
                                       score_fun=score_fun)
-            if best_score is None\
-                    or greater_is_better and score > best_score\
+            if best_score is None \
+                    or greater_is_better and score > best_score \
                     or not greater_is_better and score < best_score:
                 best_score = score
                 best_model_params = model_params
                 best_train_params = trainer_params
 
+                if print_progress:
+                    pbar.set_postfix({'Best score': best_score})
+
             if print_progress:
                 pbar.update()
-                pbar.set_postfix({'Best score': best_score})
 
     if print_progress:
         pbar.close()
 
+    experiment.set_default_trainer_args()
+    experiment.set_default_model_args()
     return best_score, best_model_params, best_train_params
