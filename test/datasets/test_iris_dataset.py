3cc6e80c22fd82b57df8142d90aa6b524ce1cd8f
Ivan Smoković <ivasmo95@gmail.com>
2019-09-16 12:09:06 +0200

    Added tests

---
+++ test/datasets/test_iris_dataset.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def test_iris_dataset():
+    sklearn_datasets = pytest.importorskip("sklearn.datasets")
+    from takepod.datasets.iris_dataset import IrisDataset
+
+    iris_ds = IrisDataset()
+
+    x, y = sklearn_datasets.load_iris(True)
+
+    assert len(iris_ds) == len(x)
+    for i in range(0, len(x), 30):
+        sepal_len, sepal_width, petal_len, petal_width = x[i]
+        species = y[i]
+
+        ex = iris_ds[i]
+
+        assert ex.sepal_length[0] == sepal_len
+        assert ex.sepal_width[0] == sepal_width
+        assert ex.petal_length[0] == petal_len
+        assert ex.petal_width[0] == petal_width
+        assert ex.species[0] == species
