47b623e709e9488339da54a339ea077ec3f3e34c
fboltuzic <filip.boltuzic@fer.hr>
2019-09-26 13:07:38 +0200

    CR comments, readme update

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
 
-setup(name='takepod',
-      version='0.0.1',
-      description='TakeLab podium (takepod) project',
-      author='TakeLab',
-      author_email='takelab@fer.hr',
-      license='MIT',
-      packages=find_packages(),
-      url="https://git.takelab.fer.hr/TakeLab/takepod",
-      setup_requires=["pytest-runner"],
-      # pytest version because of https://github.com/pytest-dev/pytest/issues/3950
-      # tests need to be rewritten
-      tests_require=["pytest==3.7.4", "pandas", "mock", "pytest_mock"],
-      install_requires=[
-        "paramiko", "tqdm", "numpy", 
-        "requests", "scipy", "sklearn", "dill"
-      ],
-      package_data={'takepod': ['preproc/stemmer/data/*.txt']},
-      zip_safe=False)
+
+# assumes no comments in requirements.txt
+# could use internal pip `from pip._internal.req import parse_requirements`
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name='takepod',
+    version='0.0.1',
+    description='TakeLab podium (takepod) project',
+    author='TakeLab',
+    author_email='takelab@fer.hr',
+    license='MIT',
+    packages=find_packages(),
+    url="https://git.takelab.fer.hr/TakeLab/takepod",
+    setup_requires=["pytest-runner"],
+    # pytest version because of https://github.com/pytest-dev/pytest/issues/3950
+    # tests need to be rewritten
+    tests_require=["pytest==3.7.4", "pandas", "mock", "pytest_mock"],
+    install_requires=install_requires,
+    package_data={'takepod': ['preproc/stemmer/data/*.txt']},
+    zip_safe=False
+)
