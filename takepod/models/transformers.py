838a90dfe5fa20401c3c30e2e316b10ffe928946
Ivan Smoković <ivasmo95@gmail.com>
2019-09-26 19:26:35 +0200

    Added some documentation

--- takepod/models/transformers.py
+++ takepod/models/transformers.py
@@ -5,17 +5,40 @@ import numpy as np
 
 
 class TensorTransformer(ABC):
-
+    """Abstract class used to transform tensors. Used in feature pre-processing during
+    training and prediction.
+    """
     @abstractmethod
     def fit(self,
             x: np.ndarray,
             y: np.ndarray):
+        """Fits the transformer to the provided data.
+
+        Parameters
+        ----------
+
+        x: np.ndarray
+            Features in numpy array form.
+        y: np.ndarray
+            Labels in numpy array form.
+        """
         pass
 
     @abstractmethod
     def transform(self,
                   x: np.array
                   ) -> np.ndarray:
+        """Transforms the passed features.
+
+        Parameters
+        ----------
+        x: np.ndarray
+            Features to be transformed in numpy array form.
+
+        Returns
+        -------
+        np.array
+            Transformed features."""
         pass
 
 
@@ -35,11 +58,25 @@ class DummyTensorTransformer(TensorTransformer):
 # TODO add mechanism for Feature transformer to know if its tensor_transformer needs
 #  fitting so batching can be avoided by callers.
 class FeatureTransformer:
-
+    """Class used to transform podium batches into features used in model prediction and
+    training."""
     def __init__(self,
                  feature_extraction_fn: Callable[[NamedTuple], np.ndarray],
                  tensor_transformer: TensorTransformer = None,
                  requires_fitting=True):
+        """Creates a new FeatureTransformer.
+
+        Parameters
+        ----------
+        feature_extraction_fn: Callable[[NamedTuple], np.ndarray]
+            Callable that takes a podium feature batch as an argument and returns a
+            numpy tensor representing the data.
+        tensor_transformer: TensorTransformer
+            TensorTransformer used to transform the transform the tensors provided by the
+            `feature_extraction_fn` callable.
+        requires_fitting: bool
+            Whether the provided TensorTransformer requires fitting.
+        """
         self.feature_extraction_fn = feature_extraction_fn
         self.tensor_transformer = tensor_transformer
         self.requires_fitting_flag = requires_fitting
@@ -47,6 +84,16 @@ class FeatureTransformer:
     def fit(self,
             x: NamedTuple,
             y: np.ndarray):
+        """Fits this tensor transformer to the provided data.
+
+        Parameters
+        ----------
+        x: NamedTuple
+            Podium feature batch containing the features to be transformed.
+
+        y: np.ndarray
+            Labels corresponding to the features in `x`.
+        """
         if not self.requires_fitting():
             return
 
@@ -55,6 +102,18 @@ class FeatureTransformer:
 
     def transform(self,
                   x: NamedTuple) -> np.ndarray:
+        """
+        Trasforms the provided podium feature batch into a numpy array.
+        Parameters
+        ----------
+        x: NamedTuple
+            Feature batch to be transformed.
+
+        Returns
+        -------
+        np.ndarray
+            Transformed features.
+        """
         x_tensor = self.feature_extraction_fn(x)
         if self.tensor_transformer is None:
             return x_tensor
