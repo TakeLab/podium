ec464add246759976cef7e84375048a0fc6436f7
enoch <sil.skud@gmail.com>
2020-02-25 18:09:40 +0100

    Fixed indentation and line length in snli test

--- test/datasets/test_snli_dataset.py
+++ test/datasets/test_snli_dataset.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 import pytest
-from takepod.datasets.impl.snli_dataset import SNLISimple, SNLIDataset
+from takepod.datasets.impl.snli_dataset import SNLISimple
 from takepod.datasets.dataset import Dataset
 from takepod.storage.resources.large_resource import LargeResource
 
@@ -9,19 +9,27 @@ from takepod.storage.resources.large_resource import LargeResource
 # TODO: Add full test examples once LabelField is merged to master.
 
 TRAIN_EXAMPLES_SIMPLE = [
-    '{"gold_label": "neutral", "sentence1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", '
-    '"sentence2": "Vestibulum vehicula, eros at bibendum convallis, nulla eros luctus dui."}',
-    '{"gold_label": "contradiction", "sentence1": "Fusce vehicula suscipit diam eu posuere. Aliquam ante diam.", '
-    '"sentence2": "Mauris eros libero, mattis quis odio ut, dictum consequat lorem."}'
+    '{"gold_label": "neutral", "sentence1": "Lorem ipsum '
+    'dolor sit amet, consectetur adipiscing elit.", '
+    '"sentence2": "Vestibulum vehicula, eros at bibendum '
+    'convallis, nulla eros luctus dui."}',
+    '{"gold_label": "contradiction", "sentence1": "Fusce '
+    'vehicula suscipit diam eu posuere. Aliquam ante diam.", '
+    '"sentence2": "Mauris eros libero, mattis quis odio ut, '
+    'dictum consequat lorem."}'
 ]
 
 EXPECTED_EXAMPLES_SIMPLE = [
     {"gold_label": "neutral",
-     "sentence1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.".split(),
-     "sentence2": "Vestibulum vehicula, eros at bibendum convallis, nulla eros luctus dui.".split()},
+     "sentence1": ("Lorem ipsum dolor sit amet, "
+                   "consectetur adipiscing elit.").split(),
+     "sentence2": ("Vestibulum vehicula, eros at bibendum "
+                   "convallis, nulla eros luctus dui.").split()},
     {"gold_label": "contradiction",
-     "sentence1": "Fusce vehicula suscipit diam eu posuere. Aliquam ante diam.".split(),
-     "sentence2": "Mauris eros libero, mattis quis odio ut, dictum consequat lorem.".split()}
+     "sentence1": ("Fusce vehicula suscipit diam eu "
+                   "posuere. Aliquam ante diam.").split(),
+     "sentence2": ("Mauris eros libero, mattis quis "
+                   "odio ut, dictum consequat lorem.").split()}
 ]
 
 
@@ -36,6 +44,7 @@ def mock_dataset_path():
     create_examples(dataset_simple, TRAIN_EXAMPLES_SIMPLE)
     return dataset_simple
 
+
 def create_examples(file_name, raw_examples):
     with open(file=file_name, mode='w', encoding="utf8") as fpr:
         for example in raw_examples:
@@ -45,11 +54,9 @@ def create_examples(file_name, raw_examples):
 def test_default_fields():
     fields = SNLISimple.get_default_fields()
     assert len(fields) == 3
-    field_names = [
-                   SNLISimple.GOLD_LABEL_FIELD_NAME,
+    field_names = [SNLISimple.GOLD_LABEL_FIELD_NAME,
                    SNLISimple.SENTENCE1_FIELD_NAME,
-                   SNLISimple.SENTENCE2_FIELD_NAME
-                  ]
+                   SNLISimple.SENTENCE2_FIELD_NAME]
     assert all([name in fields for name in field_names])
     # Label field is a target
     assert fields[SNLISimple.GOLD_LABEL_FIELD_NAME].is_target
@@ -64,5 +71,7 @@ def test_load_dataset(mock_dataset_path):
 
     assert len(dataset) == 2
     for ex in dataset:
-        ex_data = {"gold_label": ex.gold_label[0], "sentence1": ex.sentence1[1], "sentence2": ex.sentence2[1]}
+        ex_data = {"gold_label": ex.gold_label[0],
+                   "sentence1": ex.sentence1[1],
+                   "sentence2": ex.sentence2[1]}
         assert ex_data in EXPECTED_EXAMPLES_SIMPLE
