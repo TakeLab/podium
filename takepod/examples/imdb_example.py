70d42509b859554420b28f646335e9bc5fee0e30
Martin Tutek <martin.tutek@gmail.com>
2020-03-18 10:48:38 +0100

    Move nb to file

--- takepod/examples/imdb_example.py
+++ takepod/examples/imdb_example.py
@@ -1,11 +1,8 @@
-import math
-import time
-import takepod
+# flake8: noqa
 import pickle
 
 import torch
 import torch.nn as nn
-import numpy as np
 
 from takepod.models import Experiment
 from takepod.storage import LabelField, Field, Vocab
@@ -17,7 +14,7 @@ from takepod.models.impl.pytorch import TorchTrainer, TorchModel, AttentionRNN
 
 def lowercase(raw, tokenized):
     """Applies lowercasing as a post-tokenization hook
-    
+
     Parameters
     ----------
     Raw : str
@@ -26,9 +23,9 @@ def lowercase(raw, tokenized):
         list of tokens.
     Returns
     -------
-    Raw: str 
+    Raw: str
         unmodified input
-    Tokenized: list(str) 
+    Tokenized: list(str)
         lowercased tokenized data
     """
     return raw, [token.lower() for token in tokenized]
@@ -36,7 +33,7 @@ def lowercase(raw, tokenized):
 
 def max_length(raw, data, length=200):
     """Applies lowercasing as a post-tokenization hook
-    
+
     Parameters
     ----------
     Raw : str
@@ -47,9 +44,9 @@ def max_length(raw, data, length=200):
         maximum length for each instance 
     Returns
     -------
-    Raw: str 
+    Raw: str
         unmodified input
-    Tokenized: list(str) 
+    Tokenized: list(str)
         tokenized data truncated to `length`
     """
     return raw, data[:length]
@@ -68,21 +65,9 @@ def create_fields():
     # 2. Truncate to length
     text.add_posttokenize_hook(max_length)
 
-    label = LabelField(name='label', vocab = Vocab(specials=()))
-    return {text.name : text, label.name: label}
+    label = LabelField(name='label', vocab=Vocab(specials=()))
+    return {text.name: text, label.name: label}
 
-
-def create_fields():
-    # Define the vocabulary
-    vocab = Vocab(max_size=10000, min_freq=5)
-    text = Field(name='text', vocab=vocab, tokenizer='spacy', store_as_raw=False)
-    # Add preprpocessing hooks to model
-    # 1. Lowercase
-    text.add_posttokenize_hook(lowercase)
-    text.add_posttokenize_hook(max_length)
-    # Improve readability: LabelField
-    label = Field(name='label', vocab=Vocab(specials=()), is_target=True, tokenize=False)
-    return {text.name : text, label.name: label}
 
 def main():
     fields = create_fields()
@@ -93,7 +78,7 @@ def main():
     embeddings = GloVe().load_vocab(vocab)
     print(f"For vocabulary of size: {len(vocab)} loaded embedding matrix of shape: {embeddings.shape}")
 
-    # First, we will define the hyperparameters for our model. 
+    # First, we will define the hyperparameters for our model.
     # These are only used when a concrete model is trained, and can be changed between calls.
     model_config = {
         'rnn_type': 'LSTM',
@@ -106,7 +91,6 @@ def main():
         'batch_size': 32,
         'dropout': 0.,
         'bidirectional': True,
-        'gpu': -1
     }
 
     # Task-specific metadata
@@ -115,7 +99,7 @@ def main():
     model_config['vocab_size'] = len(vocab)
     model_config['pretrained_embedding'] = embeddings
     # Run on CPU since we don't have a GPU on this machine
-    device = torch.device('gpu:0')
+    device = torch.device('cuda:0')
     # Define the model criterion
     criterion = nn.CrossEntropyLoss()
 
@@ -145,19 +129,18 @@ def main():
     with open(model_save_file, 'rb') as load_file:
         loaded_model = pickle.load(load_file)
 
-
     ft = experiment.feature_transformer
-    cast_to_torch_transformer = lambda t: torch.from_numpy(ft.transform(t).swapaxes(0,1)).to(device)
+    cast_to_torch_transformer = lambda t: torch.from_numpy(ft.transform(t).swapaxes(0, 1)).to(device)
 
     pipe = Pipeline(
-      fields = list(fields.values()),
-      example_format = 'list',
-      feature_transformer = cast_to_torch_transformer,
-      model = fitted_model
+      fields=list(fields.values()),
+      example_format='list',
+      feature_transformer=cast_to_torch_transformer,
+      model=fitted_model
       )
 
     instances = [
-            ['This movie is horrible'], 
+            ['This movie is horrible'],
             ['This movie is great!']
     ]
 
@@ -166,6 +149,7 @@ def main():
         print(f"For instance: {instance}, the prediction is: "
               f"{fields['label'].vocab.itos[prediction.argmax()]},"
               f" with logits: {prediction}")
+
 
 if __name__ == '__main__':
   main()
