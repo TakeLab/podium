b7710cb2c2052a835b0b7256961c8a1c769f754d
Martin Tutek <martin.tutek@gmail.com>
2020-03-18 10:40:11 +0100

    Move nb to file

--- takepod/examples/imdb_example.py
+++ takepod/examples/imdb_example.py
@@ -1,28 +1,76 @@
-# flake8: noqa
-# For now; code will change a lot
 import math
 import time
-import pickle
 import takepod
-
-from takepod.datasets import BucketIterator, Iterator, IMDB
-from takepod.storage import Field, Vocab
-from takepod.storage.vectorizers.impl import GloVe
-from takepod.models import Experiment, AbstractSupervisedModel
-from takepod.models.trainer import AbstractTrainer
-from takepod.pipeline import Pipeline
-
-from takepod.models.impl.sequence_classification import *
+import pickle
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import numpy as np
 
-def lowercase(raw, data):
-    return raw, [d.lower() for d in data]
+from takepod.models import Experiment
+from takepod.storage import LabelField, Field, Vocab
+from takepod.pipeline import Pipeline
+from takepod.datasets import IMDB, Iterator
+from takepod.storage.vectorizers.impl import GloVe
+from takepod.models.impl.pytorch import TorchTrainer, TorchModel, AttentionRNN
+
+
+def lowercase(raw, tokenized):
+    """Applies lowercasing as a post-tokenization hook
+    
+    Parameters
+    ----------
+    Raw : str
+        the untokenized input data
+    Tokenized: list(str)
+        list of tokens.
+    Returns
+    -------
+    Raw: str 
+        unmodified input
+    Tokenized: list(str) 
+        lowercased tokenized data
+    """
+    return raw, [token.lower() for token in tokenized]
+
 
 def max_length(raw, data, length=200):
+    """Applies lowercasing as a post-tokenization hook
+    
+    Parameters
+    ----------
+    Raw : str
+        the untokenized input data
+    Tokenized: list(str)
+        list of tokens.
+    Length: int
+        maximum length for each instance 
+    Returns
+    -------
+    Raw: str 
+        unmodified input
+    Tokenized: list(str) 
+        tokenized data truncated to `length`
+    """
     return raw, data[:length]
+
+
+def create_fields():
+    # Define the vocabulary
+    max_vocab_size = 10000
+    min_frequency = 5
+    vocab = Vocab(max_size=max_vocab_size, min_freq=min_frequency)
+
+    text = Field(name='text', vocab=vocab, tokenizer='spacy', store_as_raw=False)
+    # Add preprpocessing hooks to model
+    # 1. Lowercase
+    text.add_posttokenize_hook(lowercase)
+    # 2. Truncate to length
+    text.add_posttokenize_hook(max_length)
+
+    label = LabelField(name='label', vocab = Vocab(specials=()))
+    return {text.name : text, label.name: label}
+
 
 def create_fields():
     # Define the vocabulary
@@ -38,16 +86,16 @@ def create_fields():
 
 def main():
     fields = create_fields()
-    imdb_train, imdb_test = IMDB.get_train_test_dataset(fields)
+    imdb_train, imdb_test = IMDB.get_dataset_splits(fields)
 
     # Construct vectoziter based on vocab
     vocab = fields['text'].vocab
-    #embeddings = GloVe().load_vocab(vocab)
+    embeddings = GloVe().load_vocab(vocab)
+    print(f"For vocabulary of size: {len(vocab)} loaded embedding matrix of shape: {embeddings.shape}")
 
-    criterion = nn.CrossEntropyLoss()
-    label_vocab = fields['label'].vocab
-    # Ugly but just to check
-    config_dict = {
+    # First, we will define the hyperparameters for our model. 
+    # These are only used when a concrete model is trained, and can be changed between calls.
+    model_config = {
         'rnn_type': 'LSTM',
         'embed_dim': 300,
         'hidden_dim': 150,
@@ -58,28 +106,32 @@ def main():
         'batch_size': 32,
         'dropout': 0.,
         'bidirectional': True,
-        'cuda': False,
-        'vocab_size': len(vocab),
-        'num_classes': len(label_vocab),
+        'gpu': -1
     }
 
-    device = torch.device('cuda:0')
-    config = Config(config_dict)
+    # Task-specific metadata
+    label_vocab = fields['label'].vocab
+    model_config['num_classes'] = len(label_vocab)
+    model_config['vocab_size'] = len(vocab)
+    model_config['pretrained_embedding'] = embeddings
+    # Run on CPU since we don't have a GPU on this machine
+    device = torch.device('cpu:0')
+    # Define the model criterion
+    criterion = nn.CrossEntropyLoss()
 
     data_iterator = Iterator(batch_size=32)
 
-    trainer = TorchTrainer(config.epochs, device, data_iterator, imdb_test)
+    trainer = TorchTrainer(model_config['epochs'], device, data_iterator, imdb_test)
+    experiment = Experiment(TorchModel, trainer=trainer)
 
-    experiment = Experiment(MyTorchModel, trainer=trainer)
     model = experiment.fit(
-        imdb_train,
-        model_kwargs={
-            'model_class': AttentionRNN, 
-            'config': config, 
-            'criterion': criterion,
-            'optimizer': torch.optim.Adam,
-            'device': device
-            #'embedding': embedding
+        imdb_train,  # Data on which to fit the model
+        model_kwargs={  # Arguments passed to the model constructor
+            'model_class': AttentionRNN,  # The wrapped concrete model
+            'criterion': criterion,  # The loss for the concrete model
+            'optimizer': torch.optim.Adam,  # Optimizer _class_
+            'device': device,  # The device to store the data on
+            **model_config  # Delegated to the concrete model
         },
     )
 
@@ -88,10 +140,11 @@ def main():
 
     model_save_file = 'model.pt'
     with open(model_save_file, 'wb') as dump_file:
-      pickle.dump(fitted_model, dump_file)
+        pickle.dump(fitted_model, dump_file)
 
     with open(model_save_file, 'rb') as load_file:
-      loaded_model = pickle.load(load_file)
+        loaded_model = pickle.load(load_file)
+
 
     ft = experiment.feature_transformer
     cast_to_torch_transformer = lambda t: torch.from_numpy(ft.transform(t).swapaxes(0,1)).to(device)
@@ -103,10 +156,16 @@ def main():
       model = fitted_model
       )
 
-    instance = ['This movie is horrible']
-    prediction = pipe.predict_raw(instance)
-    print(f"For instance: {instance}, the prediction is: {fields['label'].vocab.itos[prediction.argmax()]}, with logits: {prediction}")
-    #print(prediction.argmax())
+    instances = [
+            ['This movie is horrible'], 
+            ['This movie is great!']
+    ]
+
+    for instance in instances:
+        prediction = pipe.predict_raw(instance)
+        print(f"For instance: {instance}, the prediction is: "
+              f"{fields['label'].vocab.itos[prediction.argmax()]},"
+              f" with logits: {prediction}")
 
 if __name__ == '__main__':
   main()
