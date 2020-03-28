5997d8daa950b6ffde58dce513fea7fdf893af3f
Martin Tutek <martin.tutek@gmail.com>
2020-03-28 12:45:50 +0100

    Flake

--- takepod/datasets/impl/sst_sentiment_dataset.py
+++ takepod/datasets/impl/sst_sentiment_dataset.py
@@ -60,7 +60,7 @@ class SST(Dataset):
             LargeResource.URI: SST.URL})
         # Assign these to enable filtering
         self.examples = self._create_examples(file_path=file_path, fields=fields,
-                                              fine_grained=fine_grained, 
+                                              fine_grained=fine_grained,
                                               subtrees=subtrees)
         self.fields = fields
 
