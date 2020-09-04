4811b5ba7c5bc21415a6c4db97878b33dda363f2
Ivan Smokovic <ivasmo95@gmail.com>
2020-09-04 21:40:03 +0200

    WIP added slicing to arrow dataset

--- podium/arrow/arrow_tabular_dataset.py
+++ podium/arrow/arrow_tabular_dataset.py
@@ -2,13 +2,17 @@ import pyarrow as pa
 import tempfile
 import itertools
 from os import path
-from collections import defaultdict
+import shutil
+import logging
+import pickle
 
 from podium.datasets import Dataset
 from podium.storage.example_factory import Example
 
+_LOGGER = logging.getLogger(__name__)
 
-def group(n, iterable):
+
+def group(iterable, n):
     # groups an iterable into tuples of size n
     it = iter(iterable)
     while True:
@@ -19,46 +23,72 @@ def group(n, iterable):
 
 
 class ArrowDataset(Dataset):
-    CACHE_FILENAME = 'arrow_cache'
-    CHUNK_SIZE = 100
+    TEMP_CACHE_FILENAME_PREFIX = 'podium_arrow_cache'
+    CACHE_TABLE_FILENAME = 'podium_arrow_cache.arrow'
+    CACHE_FIELDS_FILENAME = 'podium_fields.pkl'
+    CHUNK_MAX_SIZE = 1000
 
-    def __init__(self, fields, examples, cache_path=None, data_types=None):
+    def __init__(self, table, fields, cache_path, mmapped_file):
+        self.cache_path = cache_path
 
         self.fields = fields
         self.fields_dict = {field.name: field for field in fields}
 
-        if cache_path is None:
-            self.temp_dir = tempfile.TemporaryDirectory(prefix='podium_arrow_cache')
-            self.cache_file_path = path.join(self.temp_dir.name, ArrowDataset.CACHE_FILENAME)
+        self.mmapped_file = mmapped_file
 
-        else:
-            self.temp_dir = None
-            self.cache_file_path = path.join(cache_path, ArrowDataset.CACHE_FILENAME)
+        self.table = table
+
+    @staticmethod
+    def _load_from_cache_path(cache_path):
+        # load fields
+        fields_file_path = path.join(cache_path, ArrowDataset.CACHE_FIELDS_FILENAME)
+        with open(fields_file_path, 'rb') as fields_cache_file:
+            fields = pickle.load(fields_cache_file)
+
+        # load dataset as memory mapped arrow table
+        table_file_path = path.join(cache_path, ArrowDataset.CACHE_TABLE_FILENAME)
+        mmapped_file = pa.memory_map(table_file_path)
+        table = pa.RecordBatchFileReader(mmapped_file).read_all()
+        return ArrowDataset(table, fields, cache_path, mmapped_file)
+
+    @staticmethod
+    def from_examples(fields, examples, cache_path=None, data_types=None):
+
+        if cache_path is None:
+            cache_path = tempfile.mkdtemp(prefix=ArrowDataset.TEMP_CACHE_FILENAME_PREFIX)
+
+        # pickle fields
+        cache_fields_path = path.join(cache_path, ArrowDataset.CACHE_FIELDS_FILENAME)
+        with open(cache_fields_path, 'wb') as fields_cache_file:
+            pickle.dump(fields, fields_cache_file)
+
+        # dump dataset table
+        cache_table_path = path.join(cache_path, ArrowDataset.CACHE_TABLE_FILENAME)
 
         # TODO hande cache case when cache is present
 
-        chunks_iter = group(ArrowDataset.CHUNK_SIZE, examples)
+        chunks_iter = group(examples, ArrowDataset.CHUNK_MAX_SIZE)
 
         # get first group to infer schema
         first_group = chunks_iter.__next__()
-        record_batch = self._examples_to_recordbatch(first_group)
+        record_batch = ArrowDataset._examples_to_recordbatch(first_group, fields)
 
-        with pa.OSFile(self.cache_file_path, 'wb') as f:
+        with pa.OSFile(cache_table_path, 'wb') as f:
             with pa.RecordBatchFileWriter(f, schema=record_batch.schema) as writer:
                 writer.write(record_batch)  # write first chunk
                 for examples_chunk in chunks_iter:  # write rest of chunks
-                    record_batch = self._examples_to_recordbatch(examples_chunk)
+                    record_batch = ArrowDataset._examples_to_recordbatch(examples_chunk, fields)
                     writer.write(record_batch)
 
-        self.mmapped_file = pa.memory_map(self.cache_file_path)
-        self.table: pa.Table = pa.RecordBatchFileReader(self.mmapped_file).read_all()
+        return ArrowDataset(cache_path, fields)
 
-    def _examples_to_recordbatch(self, examples):
+    @staticmethod
+    def _examples_to_recordbatch(examples, fields):
         #  transpose examples
         #  TODO add explicit typing
         arrays = []
         array_names = []
-        for field in self.fields:
+        for field in fields:
             field_raw_column = []
             field_tokenized_column = []
 
@@ -75,8 +105,9 @@ class ArrowDataset(Dataset):
 
         return pa.RecordBatch.from_arrays(arrays, names=array_names)
 
-    def _recordbatch_to_examples(self, record_batch):
-        fieldnames = tuple(field.name for field in self.fields)
+    @staticmethod
+    def _recordbatch_to_examples(record_batch, fields):
+        fieldnames = tuple(field.name for field in fields)
         examples = [Example(fieldnames) for _ in range(len(record_batch))]
 
         for fieldname in fieldnames:
@@ -92,20 +123,30 @@ class ArrowDataset(Dataset):
 
         return examples
 
+    def _slice(self, slice: slice) -> 'ArrowDataset':
+        table_slice = self.table[slice]
+
+        return ArrowDataset(table=table_slice,
+                            fields=self.fields,
+                            cache_path=self.cache_path,
+                            mmapped_file=self.mmapped_file)
+
     def __getitem__(self, item, deep_copy=False):
 
         if isinstance(item, int):
-            record_batch = self.table[item:item + 1]  # indexing with int extracts column
-            return self._recordbatch_to_examples(record_batch)[0]
+            record_batch = self.table[item:item + 1]  # slices extract row, indexing with int extracts column
+            return ArrowDataset._recordbatch_to_examples(record_batch, self.fields)[0]
 
         if isinstance(item, slice):
-            record_batch = self.table[item]
+            table_slice = self.table[item]
 
         else:
-            record_batch = self.table.take(item)
+            table_slice = self.table.take(item)
 
-        examples = self._recordbatch_to_examples(record_batch)
-        return Dataset(examples, self.fields)
+        return ArrowDataset(table=table_slice,
+                            fields=self.fields,
+                            cache_path=self.cache_path,
+                            mmapped_file=self.mmapped_file)
 
     def __len__(self):
         return self.table.num_rows
@@ -117,9 +158,17 @@ class ArrowDataset(Dataset):
     # TODO __getattr__
 
     def close(self):
-        self.mmapped_file.close()
-        if self.temp_dir is not None:
-            self.temp_dir.close()
-            self.temp_dir = None
+        if self.mmapped_file is not None:
+            self.mmapped_file.close()
+            self.mmapped_file = None
+
+        else:  # Do nothing
+            msg = "Attempted closing an already closed ArrowDataset."
+            _LOGGER.debug(msg)
 
         # TODO close file handles
+
+    def delete_cache(self):
+        if self.mmapped_file is not None:
+            self.close()
+        shutil.rmtree(self.cache_path)
