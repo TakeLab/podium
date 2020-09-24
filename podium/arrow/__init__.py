d9165ae0c1b8de71616291f1246b2ee98209b45c
I van Smokovic <ivasmo95@gmail.com>
2020-09-25 01:34:31 +0200

    WIP: ArrowDataset documentation

--- podium/arrow/__init__.py
+++ podium/arrow/__init__.py
@@ -1,4 +1,4 @@
-"""Module contains podium compatible datasets that use PyArrow as the data storage backend"""
+"""Module contains PyArrow integrations for Podium"""
 
 from .arrow_tabular_dataset import ArrowDataset
 
