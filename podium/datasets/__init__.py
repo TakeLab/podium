ad4ced6228ea9e472702e20941e03334224251c6
Martin Tutek <martin.tutek@gmail.com>
2021-01-21 13:17:23 +0100

    Remove eurovoc import

--- podium/datasets/__init__.py
+++ podium/datasets/__init__.py
@@ -18,7 +18,6 @@ from .hierarhical_dataset import HierarchicalDataset
 from .impl.catacx_dataset import CatacxDataset
 from .impl.conllu_dataset import CoNLLUDataset
 from .impl.cornell_movie_dialogs_dataset import CornellMovieDialogsConversationalDataset
-from .impl.eurovoc_dataset import EuroVocDataset
 from .impl.imdb_sentiment_dataset import IMDB
 from .impl.pauza_dataset import PauzaHRDataset
 from .impl.snli_dataset import SNLIDataset, SNLISimple
