f42d9cbdd85d6d562cc8a9823e253208f4a070f1
Martin Tutek <martin.tutek@gmail.com>
2021-02-18 16:40:21 +0100

    Remove docs dependencies on torch, tf and keras

--- setup.py
+++ setup.py
@@ -22,21 +22,21 @@ DOCLINES = __doc__.split('\n')
 
 
 INSTALL_REQUIRES = [
+    # for numericalization in batching
+    "numpy<=1.19",
     # for improved dataset pickling
     "dill",
     # for tokenization and data encoded in tree structure
     "nltk>=3.0",
-    # for numericalization in batching
-    "numpy",
     # for improved csv parsing
-    "pandas",
+    "pandas<1.2.0",
     # for downloading datasets over HTTP
     "paramiko",
     "requests",
     # for models and model selection
     "scikit-learn",
     # for sparse storage
-    "scipy",
+    "scipy<1.6.0",
     # for preprocessing (tokenization, hooks, etc.)
     "spacy",
     # progress bar in download and model selection
@@ -45,7 +45,6 @@ INSTALL_REQUIRES = [
     "yake @ https://github.com/LIAAD/yake/archive/v0.4.2.tar.gz",
     # for some datasets
     'xlrd',
-
 ]
 
 
@@ -95,8 +94,8 @@ DOCS_REQUIRE = [
     'sphinx-copybutton',
     'recommonmark',
     'datasets',
-    'keras==2.2.4',
-    'tensorflow==1.15',
+#    'keras==2.2.4',
+#    'tensorflow==1.15',
     'torch',
     'xlrd',
 ]
