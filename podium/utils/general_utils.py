b1f850f944eebbb59cbe9391b06749eeba3a40a3
mariosasko <mariosasko777@gmail.com>
2021-02-20 19:58:39 +0100

    Catch AttributeError in load_spacy_model_or_raise

--- podium/utils/general_utils.py
+++ podium/utils/general_utils.py
@@ -2,26 +2,28 @@
 Module contains general utilites used throughout the codebase.
 """
 
-import warnings
-
 
 def load_spacy_model_or_raise(model, *, disable=None):
     disable = disable if disable is not None else []
     try:
         import spacy
-
-        nlp = spacy.load(model, disable=disable)
-    except OSError:
-        OLD_MODEL_SHORTCUTS = (
-            spacy.errors.OLD_MODEL_SHORTCUTS
-            if hasattr(spacy.errors, "OLD_MODEL_SHORTCUTS")
-            else {}
+        try:
+            nlp = spacy.load(model, disable=disable)
+        except OSError:
+            OLD_MODEL_SHORTCUTS = (
+                spacy.errors.OLD_MODEL_SHORTCUTS
+                if hasattr(spacy.errors, "OLD_MODEL_SHORTCUTS")
+                else {}
+            )
+            if model not in OLD_MODEL_SHORTCUTS:
+                raise
+            print(
+                f"Spacy model '{model}' not found, trying '{OLD_MODEL_SHORTCUTS[model]}'",
+            )
+            nlp = spacy.load(OLD_MODEL_SHORTCUTS[model])
+    except AttributeError:
+        print(
+            f"Spacy model '{model}' not found. Please install the model."
+            "See the docs at https://spacy.io for more information."
         )
-        if model not in OLD_MODEL_SHORTCUTS:
-            raise
-        warnings.warn(
-            f"Spacy model '{model}' not found, trying '{OLD_MODEL_SHORTCUTS[model]}'",
-            stacklevel=2,
-        )
-        nlp = spacy.load(OLD_MODEL_SHORTCUTS[model])
     return nlp
