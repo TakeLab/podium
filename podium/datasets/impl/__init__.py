7a8dea52af651f0f3d9dcf1ee13154b8f1b7aebd
mariosasko <mariosasko777@gmail.com>
2021-03-29 17:27:57 +0200

    Remove PauzaHRDataset

--- podium/datasets/impl/__init__.py
+++ podium/datasets/impl/__init__.py
@@ -6,6 +6,5 @@ from .conllu_dataset import CoNLLUDataset
 from .cornell_movie_dialogs_dataset import CornellMovieDialogsConversationalDataset
 from .imdb_sentiment_dataset import IMDB
 from .iris_dataset import IrisDataset
-from .pauza_dataset import PauzaHRDataset
 from .snli_dataset import SNLIDataset, SNLISimple
 from .sst_sentiment_dataset import SST
