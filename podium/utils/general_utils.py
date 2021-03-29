fdb68f79fbcfd953a76db135d14d68a9b66ba221
mariosasko <mariosasko777@gmail.com>
2021-03-29 16:01:55 +0200

    Style

--- podium/utils/general_utils.py
+++ podium/utils/general_utils.py
@@ -46,10 +46,15 @@ def load_spacy_model_or_raise(model, *, disable=None):
 
 
 def repr_type_and_attrs(
-    self: Any, attrs: Dict[str, Any], with_newlines: bool = False, repr_values: bool = True,
+    self: Any,
+    attrs: Dict[str, Any],
+    with_newlines: bool = False,
+    repr_values: bool = True,
 ) -> str:
     delim = ",\n" if with_newlines else ", "
-    attrs_str = delim.join(f"{k}: {repr(v) if repr_values else str(v)}" for k, v in attrs.items())
+    attrs_str = delim.join(
+        f"{k}: {repr(v) if repr_values else str(v)}" for k, v in attrs.items()
+    )
     attrs_str = (
         f"\n{textwrap.indent(attrs_str, ' ' * 4)}\n" if with_newlines else attrs_str
     )
